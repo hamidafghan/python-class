767bcccdb174b31199b80be375ff33afcf85b1f6
hamidafghan <abd.hamidafghan@gmail.com>
2022-02-01 19:27:13 +0100

    Data types

---
+++ fundemental/data_types.py
@@ -0,0 +1,26 @@
+# text -> string (str)
+# numbers -> integer, float, complex
+# boolean -> true, false, (bool)
+# Sequence -> list, tuple, range
+# set  -> set
+
+name = str("Jamal")
+age = int(24)
+isStudent = bool(True)
+subjects = set({"Math", "Java", "Python"})
+scores = range(6)
+
+# for item in scores:
+#     if(item == 4):
+#         break
+#     print(item)
+        
+# Numbers -> integer, float , complex
+
+age = 24 # integer -> int
+grade = 3.7 # float -> float
+fiscalCode = 566j # complex -> complex
+
+fixedGrade = int(grade)
+
+
