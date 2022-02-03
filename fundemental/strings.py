4c820d440f9eb373434171318268f6858d85739a
hamidafghan <abd.hamidafghan@gmail.com>
2022-02-03 19:19:43 +0100

    String basic functions

---
+++ fundemental/strings.py
@@ -0,0 +1,48 @@
+# In this file we are looking for the string properties
+# each class some functions and some properties name object of str
+# : array so mean from and to -> ms excel =sum(A1:A10)
+
+note = "This is some Kamal dummy text here and I want to find the string called Kamal"
+
+# the string is the array 
+# for item in fullname[0:5]:
+#     print(item)
+
+# check the if a string is exist in string (in)
+# if("Jamal" in note and True):
+#     print("Yeah, jamal is here")
+
+# Form the begining
+# print(note[0:10])  or print(note[:10])
+
+# From the end
+# print(note[10:len(note)]) or print(note[10:])
+
+name = "Jamal"
+lastname = "Kamal"
+
+fullname = name + " " + lastname
+
+# UPPERCASE, lowercase, Propercase = sentence case
+# print(fullname.lower())
+# print(fullname.upper())
+# print(fullname.sptip())
+# print(fullname.replace("Kamal", "Khan"))
+
+# note = note.split()
+# flag
+# isExist = False
+# for name in note:
+#     if(name == "Jamal"):
+#         isExist = True
+#         break
+
+# if(isExist):
+#     print("Yeah Jamal is here")
+# else:
+#     print("Jamal is not here")
+
+# print(fullname.split(',')) -> always it return the array
+
+print(fullname)
+
