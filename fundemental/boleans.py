402b6f018503bf43fa62f0d16bb12d12ed2f9461
hamidafghan <abd.hamidafghan@gmail.com>
2022-02-05 19:07:50 +0100

    Booleans

---
+++ fundemental/boleans.py
@@ -0,0 +1,13 @@
+name = "Jamal"
+is_student = True
+
+# conventions
+print(bool([])) # true
+
+# 0 -> false 
+# empty string -> false
+# set, array, list empty -> false
+
+# string -> true
+# any integer -> true
+# set, array, list no empty -> true
