76a36bcaecc9dabeab3adc8b43b847cfea93c72b
hamidafghan <abd.hamidafghan@gmail.com>
2022-02-07 19:35:35 +0100

    Operators

---
+++ fundemental/operators.py
@@ -0,0 +1,34 @@
+# Arithmetic operators 
+
+number1 = input("Please enter the frist number: ")
+number2 = input("Please enter the second number: ")# it needs to be casted
+
+# Addition (+)
+# print(int(number1) + int(number2))
+
+# Substraction (-)
+# print(float(number1) - float(number2))
+
+# Multiplication (*)
+# print(float(number1) * float(number2))
+
+# Division (/)
+# print(int(number1) / int(number2))
+
+# Modulus (%)
+# print(float(number1) % float(number2))
+# like odd or even
+
+# Power or exponentiotion (**)
+# print(float(number1) ** float(number2)) -> float
+
+# floor (//)
+# print(float(number1) // float(number2))
+
+
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Bitwise Operators
+
