fc32a3c9db8271b961f1ff8bd8a05a65844c59c5
hamidafghan <abd.hamidafghan@gmail.com>
2022-02-20 19:41:12 +0100

    Loops

---
+++ fundemental/conditions.py
@@ -0,0 +1,32 @@
+# Condition statement
+
+# if else if and else
+
+#  in python if and elif  and else
+
+from soupsieve import match
+
+
+number = "0741212126"
+number = number[1:3]
+
+# 77 MTN, 78 Etisalat, 79 Roshan, 74 Salaam
+# networks = {
+#     "74": "Salaam",
+#     "77": "MTN",
+#     "79": "Roshan",
+#     "78": "Etisalat"
+# }
+
+# print(networks[number])
+
+
+# Condtion statements switch
+
+# match number:
+#     case "74":
+#         print('Salaam')
+#     case "78":
+#         print("Etisalat")
+#     case _:
+#           print("No valid network")
---
+++ fundemental/loops.py
@@ -0,0 +1,28 @@
+# while loop
+# statements as long as a condition is true.
+
+
+# i = 0
+# while i < 5 :
+#     print("please try to widthdraw")
+#     i+=1
+# else:
+#     print("Done")
+
+# break and continue -> loop flow chart
+# i = 1
+# while i < 5:
+#     i += 1
+#     if i == 2:
+#         continue
+#     print(i)
+
+
+# For loop
+# used for iterating over a sequence (list, tuple, set, dictionaly, string)
+
+# students = ["A", "B", "C", "D"]
+
+# for student in students:
+#     for item in range(1,6):
+#         print(student+str(item))
